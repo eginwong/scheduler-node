c6fb14a95488a9db04ba50377f6341a18a0c184c
Keegan J. Grimminck <keegan.j.grimminck@avanade.com>
2019-07-27 15:11:50 -0400

    refactor: Removed unnecessary package file and cleaned tests

--- src/participants/test.js
+++ src/participants/test.js
@@ -2,26 +2,23 @@
 
 import * as Module from './index.js';
 
-const IT = console.log.bind(console, 'It');
+const it = console.log.bind(console, 'It');
+const IT = (name, test) => it(name, test());
 
-//with(global.window) {
+IT(`Exists`, function(){
+	return !!Module;
+});
 
-	IT(`Exists`, (function(){
-		return !!Module;
-	})());
+IT(`Has #AddParticipant() defined`, function(){
+	return Module.AddParticipant &&
+			'function' === typeof Module.AddParticipant;
+});
 
-	IT(`Has #AddParticipant() defined`, (function(){
-		return Module.AddParticipant &&
-				'function' === typeof Module.AddParticipant;
-	})());
-
-	IT(`Adds a participant`, (function(){
-		const name = 'Frank';
-		const preLength = Module.GetParticipants().length;
-		Module.AddParticipant(name);
-		const particips = Module.GetParticipants();
-		return particips.length == (preLength + 1) &&
-				particips[0] == name;
-	})());
-
-//}
+IT(`Adds a participant`, function(){
+	const name = 'Frank';
+	const preLength = Module.GetParticipants().length;
+	Module.AddParticipant(name);
+	const particips = Module.GetParticipants();
+	return particips.length == (preLength + 1) &&
+			particips[0] == name;
+});
