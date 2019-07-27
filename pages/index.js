dfe14f280f26277d357f8337e7e31aa26b352db7
Eric Wong <e.gin.wong@gmail.com>
2019-07-27 15:40:08 -0400

    feat: implement frontend baseline

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Index = () => (
+  <div>
+    <Link href="/landing">
+      <a>Create Schedule</a>
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
+);
+
+export default Index;
---
+++ pages/landing.js
@@ -0,0 +1,7 @@
+export default function Landing() {
+    return (
+      <div>
+        <p>This is the landing page</p>
+      </div>
+    );
+  }
