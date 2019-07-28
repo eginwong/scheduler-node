cc89ba45e80823afc739cb324d39332dbf2c152d
Keegan J. Grimminck <keegan.j.grimminck@avanade.com>
2019-07-28 16:40:57 -0400

    feat: converted new schedule into component and leveraged state for memberDto and participants list for updates (removing from list and adding to other)

--- pages/workflow/newschedule.js
+++ pages/workflow/newschedule.js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import Workflow from "./index";
 
 import { CapCase } from '../../src/utils/string.utils';
@@ -11,54 +12,69 @@ import Checkbox from '../../components/checkbox';
 
 const members = MemberService.GetMembers().map(m => JSON.parse(JSON.stringify(m)));
 const membersDto = members.map(m => { return { value: m, label: CapCase(m.name) }});
-const participants = [];
 
-let selected = null;
+export default class NewSchedule extends Component {
 
-export default function NewSchedule() {
-    return (
-      <Workflow>
-        <section>
-        	<label htmlFor="findParticipant">
-        		<p>Add Participant</p>
-        	</label>
-        	<Select id="findParticipant"
-        		value={selected} 
-        		onChange={ opt => participants.push(opt.value) }
-        		options={membersDto}
-        		placeholder="Type or select member name..." />
-        </section>
-        <section>
-        	<div>
-        		<button>MM/DD/YYYY ICON</button>
-        		<button>Generate Schedule</button>
-        	</div>
-        	<table>
-        		<tbody>
-        			<tr>
-        				<th></th>
-        				<th>Participants</th>
-        				<th>Last Role</th>
-        				<th>Volunteered Role</th>
-        				<th></th>
-        			</tr>
-        			{
-        				participants.map((particip, i) => <tr key={i}>
-        					<td key={'selected_' + i}><Checkbox id={particip.name + "_" + i}
-        							checked={ particip.selected }
-        							change={ b => particip.selected = b } />
-        						<span>&nbsp;{i + 1}&nbsp;</span>
-        					</td>
-        					<td key={'name_' + i}>{ CapCase(particip.name) }</td>
-        					<td key={'lastrole_'+ i}>{ particip.lastRole }</td>
-        					<td key={'volunrole_'+ i}>{ particip.volunRole }</td>
-        					<td key={'edit_' + i}><button>...</button></td>
-        				</tr>)
-        			}
-        		</tbody>
-        	</table>
-        </section>
-        <button className="btn btn-primary" onClick={() => console.log(DatabaseService.Export())}>Save and Export</button>
-      </Workflow>
-    );
+	constructor(props) {
+		super(props);
+		this.state = {
+			members: membersDto,
+			participants: []
+		};
+	}
+
+    handleSelect = opt => {
+		this.setState({
+			members: this.state.members.filter(member => member.label !== opt.label),
+			participants: this.state.participants.concat(opt.value)
+		});
+	}
+
+    render() {
+    	return (
+          <Workflow>
+            <section>
+            	<label htmlFor="findParticipant">
+            		<p>Add Participant</p>
+            	</label>
+            	<Select id="findParticipant"
+            		value={ null }
+            		onChange={ this.handleSelect }
+            		options={ this.state.members }
+            		placeholder="Type or select member name..." />
+            </section>
+            <section>
+            	<div>
+            		<button>MM/DD/YYYY ICON</button>
+            		<button>Generate Schedule</button>
+            	</div>
+            	<table>
+            		<tbody>
+            			<tr>
+            				<th></th>
+            				<th>Participants</th>
+            				<th>Last Role</th>
+            				<th>Volunteered Role</th>
+            				<th></th>
+            			</tr>
+            			{
+            				this.state.participants.map((particip, i) => <tr key={i}>
+            					<td key={'selected_' + i}><Checkbox id={particip.name + "_" + i}
+            							checked={ particip.selected }
+            							change={ b => particip.selected = b } />
+            						<span>&nbsp;{i + 1}&nbsp;</span>
+            					</td>
+            					<td key={'name_' + i}>{ CapCase(particip.name) }</td>
+            					<td key={'lastrole_'+ i}>{ particip.lastRole }</td>
+            					<td key={'volunrole_'+ i}>{ particip.volunRole }</td>
+            					<td key={'edit_' + i}><button>...</button></td>
+            				</tr>)
+            			}
+            		</tbody>
+            	</table>
+            </section>
+            <button className="btn btn-primary" onClick={() => console.log(DatabaseService.Export())}>Save and Export</button>
+          </Workflow>
+        );
+    }
 }
