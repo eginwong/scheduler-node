a12c3a2315769075f77d011953af56d542f6cbb2
Eric Wong <e.gin.wong@gmail.com>
2019-07-31 22:18:32 -0400

    feat: basic previous sessions page with styling

--- pages/workflow/previous-sessions.js
+++ pages/workflow/previous-sessions.js
@@ -1,26 +1,34 @@
 import Workflow from "./index";
 import HistoryService from "../../src/services/history.service";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 import "../../static/styles/previous-sessions.scss";
 
 export default () => {
   return (
     <Workflow>
       <h1>Previous Sessions</h1>
+
       <div>
         {HistoryService.GetHistory().map((data, i) => {
           return (
-            <div className="session--card" key={"fragment" + i}>
-              <h2 key={"date" + i}>{data.sessionDate}</h2>
-              <ul key={"list" + i}>
-                {data.roles.map((role, j) => {
-                  return (
-                    <li key={"role-name" + j}>
-                      {role.roleName} : {role.user}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+            <Card className="session--card" key={"card" + i}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {data.sessionDate}
+                </Typography>
+                <ul>
+                  {data.roles.map((role, j) => {
+                    return (
+                      <li className="session--card__participants" key={"role-name" + j}>
+                        {role.roleName} : {role.user}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </CardContent>
+            </Card>
           );
         })}
       </div>
