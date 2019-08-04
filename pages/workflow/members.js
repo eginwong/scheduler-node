19115fc3ccb8a18773d416d3ef37910638a66111
Eric Wong <e.gin.wong@gmail.com>
2019-08-04 13:04:10 -0400

    feat: add styling to members page and prevsessions

--- pages/workflow/members.js
+++ pages/workflow/members.js
@@ -1,52 +1,108 @@
-import Workflow from './index';
-
-import { CapCase } from '../../src/utils/string.utils';
-
-import MemberService from '../../src/services/member.service';
-
-import Checkbox from '../../components/checkbox';
+import Workflow from "./index";
+import { CapCase } from "../../src/utils/string.utils";
+import MemberService from "../../src/services/member.service";
+import Checkbox from "../../components/checkbox";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import SnackbarContent from "@material-ui/core/SnackbarContent";
+import "../../static/styles/members.scss";
 
 let roles, members;
 
 export default function Members() {
+  roles = MemberService.GetRoles()
+    .filter((r, i, a) => a.indexOf(r) === i)
+    .slice();
+  members = MemberService.GetMembers().map(m => JSON.parse(JSON.stringify(m)));
+  const [open, setOpen] = React.useState(false);
 
-	roles = MemberService.GetRoles().filter((r, i, a) => a.indexOf(r) === i).slice();
-	members = MemberService.GetMembers().map(m => JSON.parse(JSON.stringify(m)));
+  function Save() {
+    MemberService.UpdateMembers(members);
+    setOpen(true);
+  }
 
-    return (
-      <Workflow>
-        <section>
-        	<table>
-        		<tbody>
-	        		<tr>
-	        			<th>Members</th>
-	        			{
-	        				roles.filter((r, i, a) => a.indexOf(r) === i)
-	        					.map((role, i) => <th key={i}>{ CapCase(role) }</th>)
-	        			}
-	        		</tr>
-	        		{
-	        			members.map((member, i) => (<tr key={"member_" + i}>
-	        				<td key={"name_" + i}>{ CapCase(member.name) }</td>
-	        				{
-	        					Object.keys(member.capabilities).map((capability, j) => (
-	        						<td key={"capability_" + j}>
-	        							<Checkbox id={ member.name + "_" + capability }
-	        								checked={ !!member.capabilities[capability] }
-	        								change={ b => member.capabilities[capability] = +b } />
-	        						</td>
-	        					))
-	        				}
-	        			</tr>))
-	        		}
-        		</tbody>
-        	</table>
-        	<button onClick={Save}>Save Changes</button>
-        </section>
-      </Workflow>
-    );
-}
+  function handleClose(event) {
+    setOpen(false);
+  }
 
-function Save() {
-	MemberService.UpdateMembers(members);
+  return (
+    <Workflow>
+      <section>
+        <div className="members__container">
+          <Card className="members__card">
+            <CardContent>
+              <table>
+                <tbody>
+                  <tr className="members__card--header">
+                    <th>Members</th>
+                    {roles
+                      .filter((r, i, a) => a.indexOf(r) === i)
+                      .map((role, i) => (
+                        <th key={i}>{CapCase(role)}</th>
+                      ))}
+                  </tr>
+                  {members.map((member, i) => (
+                    <tr key={"member_" + i}>
+                      <td key={"name_" + i}>{CapCase(member.name)}</td>
+                      {Object.keys(member.capabilities).map((capability, j) => (
+                        <td key={"capability_" + j}>
+                          <Checkbox
+                            id={member.name + "_" + capability}
+                            checked={!!member.capabilities[capability]}
+                            change={b => (member.capabilities[capability] = +b)}
+                          />
+                        </td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </CardContent>
+            <CardActions className="members__card__actions">
+              <Button size="small" color="primary" onClick={Save}>
+                Save Changes
+              </Button>
+            </CardActions>
+          </Card>
+          <Snackbar
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right"
+            }}
+            open={open}
+            autoHideDuration={1000}
+            onClose={handleClose}
+          >
+            <SnackbarContent
+              id="snackbar--content"
+              aria-describedby="client-snackbar"
+              message={
+                <span>
+                  <CheckCircleIcon className="snackbar--side-icon" />
+                  Changes saved!
+                </span>
+              }
+              action={[
+                <IconButton
+                  key="close"
+                  aria-label="close"
+                  color="inherit"
+                  className="snackbar--button-icon"
+                  onClick={handleClose}
+                >
+                  <CloseIcon className="snackbar--icon" />
+                </IconButton>
+              ]}
+            />
+          </Snackbar>
+        </div>
+      </section>
+    </Workflow>
+  );
 }
--- pages/workflow/previous-sessions.js
+++ pages/workflow/previous-sessions.js
@@ -1,11 +1,14 @@
 import Workflow from "./index";
 import HistoryService from "../../src/services/history.service";
+import { CapCase } from "../../src/utils/string.utils";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import "../../static/styles/previous-sessions.scss";
 
 export default () => {
+  const DATE_OPTIONS = { year: 'numeric', month: 'long', day: 'numeric' };
+
   return (
     <Workflow>
       <h1>Previous Sessions</h1>
@@ -16,13 +19,13 @@ export default () => {
             <Card className="session--card" key={"card" + i}>
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
-                  {data.sessionDate}
+                  {new Date(data.sessionDate).toLocaleDateString("en-US", DATE_OPTIONS)}
                 </Typography>
                 <ul>
                   {data.roles.map((role, j) => {
                     return (
                       <li className="session--card__participants" key={"role-name" + j}>
-                        {role.roleName} : {role.user}
+                        {CapCase(role.roleName)} : {CapCase(role.user)}
                       </li>
                     );
                   })}
